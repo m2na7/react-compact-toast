b823fb39ef49c23055dd3fa4aebc9f3940b3c9ff
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-11 20:05:01 +0900

    chore: add code quality tools (prettier + lefthook) (#1)
    * chore: add prettier
    
    * chore: add lefthook
    
    * style: apply prettier formatting

--- src/components/toast-container.tsx
+++ src/components/toast-container.tsx
@@ -5,26 +5,26 @@ import { useToastContainer } from '../hooks/use-toast-container';
 import '../styles.css';
 
 const ToastContainer = () => {
-    const { getToastPositionGroupToRender } = useToastContainer();
-    const positionGroup = useMemo(
-        () => getToastPositionGroupToRender(),
-        [getToastPositionGroupToRender],
-    );
+  const { getToastPositionGroupToRender } = useToastContainer();
+  const positionGroup = useMemo(
+    () => getToastPositionGroupToRender(),
+    [getToastPositionGroupToRender]
+  );
 
-    return (
-        <Fragment>
-            {Array.from(positionGroup).map(([position, toasts]) => (
-                <div
-                    key={position}
-                    className={`toast-container toast-position-${position}`}
-                >
-                    {toasts.map((toastProps) => (
-                        <Toast key={toastProps.toastId} {...toastProps} />
-                    ))}
-                </div>
-            ))}
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      {Array.from(positionGroup).map(([position, toasts]) => (
+        <div
+          key={position}
+          className={`toast-container toast-position-${position}`}
+        >
+          {toasts.map((toastProps) => (
+            <Toast key={toastProps.toastId} {...toastProps} />
+          ))}
+        </div>
+      ))}
+    </Fragment>
+  );
 };
 
 export default memo(ToastContainer);
