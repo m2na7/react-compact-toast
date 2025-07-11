ac9df9258b4c50fd3ea8686ac5f6e99204f71543
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-12 03:52:16 +0900

    chore: update coverage configuration and migrate to vite-plugin-istanbul

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,19 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
-    },
-    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/component.ts',
-    viewportWidth: 1000,
-    viewportHeight: 660,
-    video: false,
-    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config);
       return config;
+    },
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
